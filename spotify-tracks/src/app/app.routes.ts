488a3ebaab6165f2dc7109605f839f307e4f19e9
vincent-utexas <144501600+vincent-utexas@users.noreply.github.com>
2024-07-12 21:50:07 -0500

    Made play components and routing

--- spotify-tracks/src/app/app.routes.ts
+++ spotify-tracks/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TracklistSelectComponent } from './modules/page-tracklist-select/tracklist-select.component';
 import { AuthRedirectComponent } from './modules/page-auth-redirect/auth-redirect.component';
+import { PlayComponent } from './modules/page-play/play.component';
 
 export const routes: Routes = [
-    {path: 'redirect', component: AuthRedirectComponent},
-    {path: 'start', component: TracklistSelectComponent},
+    {path: 'redirect', component: AuthRedirectComponent, title: 'Redirects the user after auth'},
+    {path: 'start', component: TracklistSelectComponent, title: 'User selects a tracklist'},
+    {path: 'play', component: PlayComponent, title: 'Main gameplay page'},
+    {path: '**'} // todo make 404 page
 ];
