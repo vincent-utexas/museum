686b9e986b0032f9759d78d0f08330c7ea4c3250
vincent-utexas <144501600+vincent-utexas@users.noreply.github.com>
2024-07-12 21:49:08 -0500

    Finished track selection screen

--- spotify-tracks/src/app/modules/page-tracklist-select/modules/identifier-form/identifier-form.component.ts
+++ spotify-tracks/src/app/modules/page-tracklist-select/modules/identifier-form/identifier-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,8 @@ import { ReactiveFormsModule, FormControl } from '@angular/forms';
   styleUrl: './identifier-form.component.css'
 })
 export class IdentifierFormComponent {
-  tracklistIdentifierControl = new FormControl('');
+  identifierControl = new FormControl(''); // controls the input box for a tracklist ID
+  onValidIdentifier = output<string>(); // a valid string occurred!!! (or forced), tell the form guys
 
   identifierIsValid() : boolean { // todo could use form validator or pipe, but here is a straightforward solution
     const SPOTIFY_ID_LEN = 22;
@@ -17,13 +18,14 @@ export class IdentifierFormComponent {
       + "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" 
       + "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)")
 
-    const identifier: string =  this.tracklistIdentifierControl.value as string;
+    const identifier: string =  this.identifierControl.value as string;
     return re.test(identifier) || identifier.length === SPOTIFY_ID_LEN;
   }
 
   onSubmit() : void {
     if (this.identifierIsValid()) {
-      localStorage.setItem('tracklist', this.tracklistIdentifierControl.value as string);
+      const identifier = this.identifierControl.value as string; // todo need to convert to spotify IDs only
+      this.onValidIdentifier.emit(identifier);
     }
   }
 }
--- spotify-tracks/src/app/modules/page-tracklist-select/tracklist-select.component.ts
+++ spotify-tracks/src/app/modules/page-tracklist-select/tracklist-select.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { IdentifierFormComponent } from './modules/identifier-form/identifier-form.component';
 import { SpotifyApiService } from '../../shared/services/spotify-api/spotify-api.service';
+import { StorageService } from '../../shared/services/storage/storage.service';
+import { SpotifyTracklist } from '../../shared/models/spotify-api-response.model';
 
 @Component({
   selector: 'app-tracklist-select',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, IdentifierFormComponent],
+  imports: [IdentifierFormComponent],
   templateUrl: './tracklist-select.component.html',
   styleUrl: './tracklist-select.component.css',
 })
 export class TracklistSelectComponent {
+  tracklistImgSrc: string | undefined = "https://i.scdn.co/image/ab67706f00000002578bdd86d879c9a9b3c8a299";
+  tracklist: SpotifyTracklist = this.spotifyApiService.generateDummyTracklist();
 
-  constructor (private spotifyApiService: SpotifyApiService ) { }
-  
+  constructor ( 
+    private spotifyApiService: SpotifyApiService,
+    private storage: StorageService,
+    private router: Router ) { }
+
+  async populateInterface(identifier: string) : Promise<void> {
+    this.storage.setIdentifier(identifier);
+    this.tracklist = await this.spotifyApiService.getTracklist();
+    console.log(this.tracklist.images);
+    this.tracklistImgSrc = this.tracklist.images[0].url;
+  }
+
+  routeToRandom() : void {
+    this.storage.setMode('random');
+    this.router.navigate(['/play']);
+  }
+
+  routeToAll() : void {
+    this.storage.setMode('all');
+    this.router.navigate(['/play']);
+  }
 
 }
