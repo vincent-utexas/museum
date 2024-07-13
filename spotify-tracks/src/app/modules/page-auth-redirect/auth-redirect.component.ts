fc0b7f815b5de910f7f623db40098d3a6af03a96
vincent-utexas <144501600+vincent-utexas@users.noreply.github.com>
2024-07-13 17:11:41 -0500

    redirect update

--- spotify-tracks/src/app/modules/page-auth-redirect/auth-redirect.component.ts
+++ spotify-tracks/src/app/modules/page-auth-redirect/auth-redirect.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 
 import { TokenService } from '../../shared/services/token/token.service';
+import { StorageService } from '../../shared/services/storage/storage.service';
 
 @Component({
   selector: 'app-auth-redirect',
@@ -13,10 +14,20 @@ import { TokenService } from '../../shared/services/token/token.service';
   `,
   styleUrl: './auth-redirect.component.css'
 })
-export class AuthRedirectComponent {
+export class AuthRedirectComponent implements OnInit {
 
-  constructor (private tokenService: TokenService) { 
-    this.tokenService.getAccessToken();
+  constructor ( 
+    private tokenService: TokenService,
+    private storage: StorageService,
+    private router: Router ) { }
+
+  ngOnInit(): void {
+      this.tokenService.getAccessToken();
+      if ('access_token' in this.storage.getItems()) {
+        this.router.navigate(['/start']); // todo fix auth setting access token
+      } else {
+        this.router.navigate(['/error']); // todo make error page
+      }
   }
 
 
